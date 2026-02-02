6421970efb669712bdbc6ff8032912421c227f70
shubhmate <shubhammate0604@gmail.com>
2026-02-02 13:41:39 +0530

    Add To-Do Page frontend form in master_1

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 import json
-from flask import Flask, jsonify, request, redirect, render_template
+from flask import Flask, jsonify, render_template
 from pymongo import MongoClient
 
 app = Flask(__name__)
-
-# Setup MongoDB connection
-client = MongoClient("")
-db = client["todo_db"]
-collection = db["items"]
 
 # Helper function to read the backend file
 def read_backend_data():
@@ -23,20 +18,6 @@ def get_data():
     data = read_backend_data()
     # Return the data as a JSON response
     return jsonify(data)
-
-@app.route('/submit_todoitem', methods=['POST'])
-def submit_todo():
-    # Get data from the form
-    name = request.form.get('item_name')
-    description = request.form.get('item_description')
-
-    # Store in MongoDB using insert_one
-    collection.insert_one({
-        "item_name": name,
-        "item_description": description
-    })
-
-    return "Item saved successfully!", 201
 
 @app.route('/')
 def todo_page():
